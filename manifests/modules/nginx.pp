62f361dd168567433646c0e500531e7a7e1323e8
Siebren Zwerver <siebren.zwerver@gmail.com>
2014-10-13 10:59:33 +0200

    fixed misplaced ensure_newline

--- manifests/modules/nginx.pp
+++ manifests/modules/nginx.pp
@@ -2,26 +2,24 @@
 # Enables the nginx module of the Panopta Agent
 define panopta::modules::nginx($url = $title) {
 	concat {"$::panopta_agent_config":
-		ensure	=> present
+		ensure				 => present,
+		ensure_newline => true,
 	}
 
 	concat::fragment { 'current config':
 		target				 => "$::panopta_agent_config",
-		ensure_newline => true,
 		content				 => "$::panopta_current_agent_config",
 		order					 => '01'
 	}
 
 	concat::fragment { 'nginx head':
 		target				 => "$::panopta_agent_config",
-		ensure_newline => true,
 		content				 => '[nignx]',
 		order					 => '02'
 	}
 
 	concat::fragment { 'url':
 		target	       => "$::panopta_agent_config",
-		ensure_newline => true,
 		content				 => "${url}",
 		order		       => '03'
 	}
