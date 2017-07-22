96988ab00f0a8aaa9ea27fbd89cc8bf1d45c5ae1
Siebren Zwerver <siebren.zwerver@gmail.com>
2017-07-22 21:48:06 +0200

    Fix: Error: Evaluation Error: Error while evaluating a Resource Statement, Panopta::Module[mysql]: expects a value for parameter 'ensure'

--- manifests/module.pp
+++ manifests/module.pp
@@ -3,23 +3,22 @@
 #
 
 define panopta::module (
-  Variant[Hash]                      $params,
-  Variant[Enum['present','abent']]   $ensure,
-  Variant[String]                    $module_name = $title,
+  Variant[Hash]   $params,
+  Variant[String] $module_name = $title,
 ) {
   include panopta::params
 
   file_line {$module_name:
-    path   => $panopta::params::agent_config,
-    line   => $module_name
+    path => $panopta::params::agent_config,
+    line => $module_name
   }
 
   $params.each |String $key, String $value| {
     file_line {"${module_name}-${key}":
-      path   => $panopta::params::agent_config,
-      line   => "${key}=${value}",
-      match  => "^${key}=",
-      after  => regsubst($module_name, '\[([^\]]+)', '\[\1\\')
+      path  => $panopta::params::agent_config,
+      line  => "${key}=${value}",
+      match => "^${key}=",
+      after => regsubst($module_name, '\[([^\]]+)', '\[\1\\')
     }
   }
 
