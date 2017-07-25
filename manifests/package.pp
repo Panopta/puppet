1dd6090ce7d944e9f26d36cfa5581a37eb9b5baa
Siebren Zwerver <siebren.zwerver@gmail.com>
2017-07-25 18:48:08 +0200

    Fix: Apt update is already called by apt module, no need to call it again

--- manifests/package.pp
+++ manifests/package.pp
@@ -11,13 +11,6 @@ class panopta::package {
     key      => {
       'id'     => '1C4698D2DE92D6EE8A769526B7F965468997217A',
       'source' => 'http://packages.panopta.com/panopta.pub'
-    },
-    notify   => Exec['panopta_apt_update']
-  }
-
-  exec { 'panopta_apt_update':
-    command     => 'apt update',
-    path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-    refreshonly => true,
+    }
   }
 }
